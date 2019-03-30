dedc101051449bf93f39f74877b27d6fcf7f83e8
Phuong Dam <phuonggiga@gmail.com>
2019-03-30 11:15:23 +0700

    Finish: Handling Iframe, Pop up, Windows

--- src/selenium/Topic07_Iframe_WindowPopUp.java
+++ src/selenium/Topic07_Iframe_WindowPopUp.java
@@ -1,6 +1,7 @@
 package selenium;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -34,7 +35,7 @@ public class Topic07_Iframe_WindowPopUp {
 	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   }
 
-  @Test
+  //@Test
   public void TC01() {
 	  driver.get("http://hdfcbank.com");
 	  /*Tại sao phải wait 10 giây? 
@@ -45,6 +46,7 @@ public class Topic07_Iframe_WindowPopUp {
 	   */
 	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	  List <WebElement> notificationIframe = driver.findElements(By.xpath("//iframe[@id='vizury-notification-template']"));
+	  //	Để so sánh với 0 thì biến cần phải ở dạng int hoặc float. Ở đây ta khai báo nó là int
 	  int notificationIframeSize = notificationIframe.size();
 	  System.out.println("So luong iframe tim duoc la " + notificationIframe.size());
 	  
@@ -111,27 +113,164 @@ public class Topic07_Iframe_WindowPopUp {
 	  
   }
   
+  
   public boolean isImageLoadedSuccess(WebElement imageElement) {
 	  return (boolean) js.executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && arguments[0].naturalWidth > 0", imageElement);
   }
   
+  //	Switch khi có 2 window/tab
+  public void switchToChildWindowByID(String parentID) {
+	  //	lấy ra ID của tất cả các cửa sổ đang có
+      Set<String> allWindows = driver.getWindowHandles();
+      
+      /* dùng for thì như này:
+      for (int i = 0; i < allWindows.size(); i++)	{
+       }
+      */
+      
+      //	dùng for-each để duyệt qua từng cửa sổ
+      for (String runWindow : allWindows) {
+    	  
+    	  		  //	kiểm tra ID của cửa sổ nào mà KHÁC với ID của parent thì switch qua
+                  if (!runWindow.equals(parentID)) {
+                              driver.switchTo().window(runWindow);
+                              break;
+                  }
+      }
+  }
+  
+  //	Switch khi có nhiều window/tab
+  public void switchToWindowByTitle(String expectedTitle) {
+	  // lấy ra ID của tất cả cửa sổ và gán cho allWindows
+      Set<String> allWindows = driver.getWindowHandles();
+      
+      // dùng vòng lặp duyệt qua từng cửa sổ
+      for (String runWindows : allWindows) {
+    	  
+    	  		  // switch qua từng cửa sổ
+                  driver.switchTo().window(runWindows);
+                  
+                  // lấy ra title của cửa sổ hiện tại đang đứng
+                  String currentWin = driver.getTitle();
+                  
+                  // nếu cửa sổ hiện tại đang đứng có title bằng với expect thì break
+                  if (currentWin.equals(expectedTitle)) {
+                              break;
+                  }
+      }
+  }
+  
+  // Đóng tất cả window / tab trừ parent. Kiểu boolean để về sau còn AssertTrue kiểm tra
+  public boolean closeAllWithoutParentWindows(String parentID) {
+	  //	lấy ra ID tất cả các cửa sổ rồi gán cho allWindows
+      Set<String> allWindows = driver.getWindowHandles();
+      
+      // dùng vòng lặp duyệt qua từng cửa sổ
+      for (String runWindows : allWindows) {
+    	  
+    	  		  // kiểm tra xem ID của child mà KHÁC ID của parent thì switch qua thằng con rồi đóng nó
+                  if (!runWindows.equals(parentID)) {
+                              driver.switchTo().window(runWindows);
+                              driver.close();
+                  }
+      }
+      driver.switchTo().window(parentID);
+      if (driver.getWindowHandles().size() == 1)
+                 return true;
+      else
+                 return false;
+  }
+  
+  
   //@Test
-  public void TC02() {
+  public void TC02() throws Exception {
+	  driver.get("https://daominhdam.github.io/basic-form/index.html");
+	  String parentID = driver.getWindowHandle();
+	  System.out.println("Parent ID = " + parentID);
 	  
+	  driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+	  Thread.sleep(2000);
+	  switchToChildWindowByID(parentID);
 	  
+	  String googleTitle = driver.getTitle();
+	  Assert.assertEquals(googleTitle, "Google");
+	  
+	  Assert.assertTrue(closeAllWithoutParentWindows(parentID));
+	  Thread.sleep(2000);
+	  
+	  Assert.assertEquals(driver.getTitle(), "SELENIUM WEBDRIVER FORM DEMO");
   }
   
   //@Test
   public void TC03() {
+	  driver.get("http://www.hdfcbank.com/");
+	  String parentID03 = driver.getWindowHandle();
 	  
+	  // Step 02 - Kiểm tra và close quảng cáo nếu có xuất hiện
+	  List <WebElement> popUps = driver.findElements(By.xpath("//iframe[@id='vizury-notification-template']"));
+	  int popUpsize = popUps.size();
+	  if(popUpsize > 0) {
+		  driver.switchTo().frame(popUps.get(0));
+		  System.out.println("There is a pop up appears!");
+		  
+		  // Kiểm tra xem cái image xuất hiện thì mình đang làm đúng
+		  Assert.assertTrue(driver.findElement(By.xpath("//div[@id='container-div']/img")).isDisplayed());
+		  
+		  // Close cái pop up
+		  js.executeScript("arguments[0].click()", driver.findElement(By.xpath("//div[@id='div-close']")));
+	  }
 	  
+	  driver.switchTo().parentFrame();
+	  
+	  // Step 03 - Click Angri link -> Mở ra tab/window mới -> Switch qua tab mới
+	  driver.findElement(By.xpath("//li//a[text()='Agri']")).click();
+	  switchToWindowByTitle("HDFC Bank Kisan Dhan Vikas e-Kendra");
+	  
+	  // Step 04 - Click Account Details link -> Mở ra tab/window mới -> Switch qua tab mới
+	  driver.findElement(By.xpath("//div/p[text()='Account Details']")).click();
+	  switchToWindowByTitle("Welcome to HDFC Bank NetBanking");
+	  
+	  // Step 05- Click Privacy Policy link (nằm trong frame) -> Mở ra tab/window mới -> Switch qua tab mới
+	  driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='footer']")));
+	  driver.findElement(By.xpath("//a[contains(text(),'Privacy Policy')]")).click();
+	  switchToWindowByTitle("HDFC Bank - Leading Bank in India, Banking Services, Private Banking, Personal Loan, Car Loan");
+	  
+	  // Step 06- Click CSR link on Privacy Policy page
+	  driver.findElement(By.xpath("//li//a[text()='CSR']"));
+	  
+	  // Step 07 - Close tất cả windows/ tabs khác - chỉ giữ lại parent window (http://www.hdfcbank.com/)
+	  Assert.assertTrue(closeAllWithoutParentWindows(parentID03));  
   }
   
-  
-  
+  @Test
+  public void TC04() {
+	  driver.get("http://live.guru99.com/index.php/");
+	  String parentID04 = driver.getWindowHandle();
+	  driver.findElement(By.xpath("//a[text()='Mobile']")).click();
+	  
+	  // Step 03 - Add sản phẩm Sony Xperia vào để Compare (Add to Compare)
+	  driver.findElement(By.xpath("//a[@title='Xperia']/following-sibling::div[@class='product-info']//a[text()='Add to Compare']")).click();
+
+	  // Step 04 - Add sản phẩm Samsung Galaxy vào để Compare (Add to Compare)
+	  driver.findElement(By.xpath("//a[@title='Samsung Galaxy']/following-sibling::div[@class='product-info']//a[text()='Add to Compare']")).click();
+	  
+	  // Step 05 - Click to Compare button
+	  driver.findElement(By.xpath("//button[@title='Compare']")).click();
+	  
+	  // Step 06 - Switch qa cửa sổ mới (chứa 2 sản phẩm đã được Add vào để Compare)
+	  switchToWindowByTitle("Products Comparison List - Magento Commerce");
+	  
+	  // Step 07 - Verify title của cửa sổ bằng: Products Comparison List - Magento Commerce
+	  Assert.assertEquals(driver.getTitle(), "Products Comparison List - Magento Commerce");
+	  
+	  // Close tab và chuyển về Parent Window
+	  Assert.assertTrue(closeAllWithoutParentWindows(parentID04));
+	  
+	  driver.navigate().back();
+  }
   @AfterTest
   public void afterTest() {
-	  driver.quit();
+	  //driver.quit();
   }
 
 }
